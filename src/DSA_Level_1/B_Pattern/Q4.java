f3df71b61cd7db964101c9dfa44277fe8d2c8e80
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-06-05 09:55:30 +0530

    Updated files Q4 and Q5

---
+++ src/DSA_Level_1/B_Pattern/Q4.java
@@ -0,0 +1,24 @@
+package DSA_Level_1.B_Pattern;
+
+import java.util.*;
+
+public class Q4 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int spaces = 0, stars = n;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= spaces; j++)
+                System.out.print("\t");
+            for(int j = 1; j <= stars; j++)
+                System.out.print("*\t");
+
+            System.out.println();
+            spaces++;
+            stars--;
+        }
+
+    }
+}
