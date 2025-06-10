0dae6b78562744e2edf0ef2366f3d84dfa0c0738
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-06-10 23:13:02 +0530

    Updated files Q19 and Q20

---
+++ src/DSA_Level_1/B_Pattern/Q20.java
@@ -0,0 +1,22 @@
+package DSA_Level_1.B_Pattern;
+
+import java.util.*;
+
+public class Q20 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if((j == 1) || (j == n) || (i > n/2 && i + j == n + 1) || (i > n/2 && i == j))
+                    System.out.print("*	");
+                else System.out.print("	");
+            }
+            System.out.println();
+        }
+    }
+}
