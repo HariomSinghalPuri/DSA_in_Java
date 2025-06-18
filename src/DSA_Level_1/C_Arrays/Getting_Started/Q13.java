3bc2fa79cced7700aa6e6ea04541b867e5125fd5
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-06-18 20:26:45 +0530

    Updated Arrays getting Started Q13

---
+++ src/DSA_Level_1/C_Arrays/Getting_Started/Q13.java
@@ -0,0 +1,35 @@
+package DSA_Level_1.C_Arrays.Getting_Started;
+
+// Linear Search
+
+import java.io.*;
+import java.util.*;
+
+public class Q13{
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<=n-1; i++)
+        {
+            arr[i] = scn.nextInt();
+        }
+
+        int d = scn.nextInt();
+
+        int ans = -1;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i] == d)
+            {
+                ans = i;
+                break;
+            }
+        }
+        System.out.print(ans);
+
+    }
+
+}
