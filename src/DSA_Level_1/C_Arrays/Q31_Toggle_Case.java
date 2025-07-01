ca96bb4e159f552638afd77bb9934c1b3578c591
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-07-01 21:33:39 +0530

    Updated Arrays Q31

---
+++ src/DSA_Level_1/C_Arrays/Q31_Toggle_Case.java
@@ -0,0 +1,31 @@
+package DSA_Level_1.C_Arrays;
+
+import java.util.*;
+
+public class Q31_Toggle_Case {
+
+    public static String toggleCase(String str){
+        String ans = "";
+
+        for(int i=0; i<str.length(); i++)
+        {
+            if(str.charAt(i) >= 'A'
+                    && str.charAt(i) <= 'Z') // uppercase
+            {
+                ans += (char)(str.charAt(i) + (32));
+            }
+            else
+            {
+                ans += (char)(str.charAt(i) - (32));
+            }
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        System.out.println(toggleCase(str));
+    }
+
+}
