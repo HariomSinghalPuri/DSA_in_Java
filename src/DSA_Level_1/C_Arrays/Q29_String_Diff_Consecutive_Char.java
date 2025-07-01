06b4c75728b36fc0f26ccd7d732fd496ee05eaa6
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-07-01 21:31:38 +0530

    Updated Arrays Q29

---
+++ src/DSA_Level_1/C_Arrays/Q29_String_Diff_Consecutive_Char.java
@@ -0,0 +1,25 @@
+package DSA_Level_1.C_Arrays;
+
+import java.util.*;
+
+public class Q29_String_Diff_Consecutive_Char {
+
+    public static String solution(String str){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<str.length()-1; i++)
+        {
+            sb.append(str.charAt(i)); // append first character in pair
+            sb.append(str.charAt(i+1) - str.charAt(i)); // append difference in ASCII values in pair
+        }
+
+        sb.append(str.charAt(str.length() - 1)); // append last character (corner case)
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        System.out.println(solution(str));
+    }
+
+}
