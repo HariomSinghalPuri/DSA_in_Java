0392968006b27811e2a5315b8b115429eb8facf9
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-07-10 21:57:33 +0530

    updated

--- src/DSA_Level_1/D_Searching_and_Sorting/E_Partitioning_Problems/Q2_Sort012.java
+++ src/DSA_Level_1/D_Searching_and_Sorting/E_Partitioning_Problems/Q2_Sort012.java
@@ -2,7 +2,7 @@ package DSA_Level_1.D_Searching_and_Sorting.E_Partitioning_Problems;
 
 import java.util.*;
 
-public class Q2_Sort02 {
+public class Q2_Sort012 {
 
     public static void sort012(int[] arr){
         int i = 0, j = 0, k = arr.length - 1;
