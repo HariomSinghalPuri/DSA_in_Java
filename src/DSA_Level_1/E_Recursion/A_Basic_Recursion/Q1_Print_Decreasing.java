51318c47488d2932ae34eb243b4f67dadbe3c6d3
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-07-12 22:44:39 +0530

    updated

--- src/DSA_Level_1/E_Recursion/A_Basic_Recursion/Q1_Print_Decreasing.java
+++ src/DSA_Level_1/E_Recursion/A_Basic_Recursion/Q1_Print_Decreasing.java
@@ -1,4 +1,25 @@
 package DSA_Level_1.E_Recursion.A_Basic_Recursion;
 
+import java.util.*;
+
 public class Q1_Print_Decreasing {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printDecreasing(n);
+    }
+
+    public static void printDecreasing(int n){
+        // Base Case
+        if(n == 0) return;
+
+        // 1st statement -> Work -> Print
+        System.out.println(n);
+
+        // 2nd statement -> Faith
+        printDecreasing(n - 1);
+    }
+
 }
