b185603345566d46eb079eff238f7d355e3ccc8f
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-07-18 00:18:32 +0530

    updated

---
+++ src/DSA_Level_1/E_Recursion/A_Basic_Recursion/Q4_Power_Linear.java
@@ -0,0 +1,30 @@
+package DSA_Level_1.E_Recursion.A_Basic_Recursion;
+
+import java.util.*;
+
+public class Q4_Power_Linear {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+
+        int xpn = power(x, n);
+        System.out.println(xpn);
+    }
+
+    public static int power(int x, int n){
+        // Base Case: x^0 = 1
+        if(n == 0) return 1;
+
+        // 1. Calculate x ^ n-1 using Faith
+        int xpn1 = power(x, n-1);
+
+        // 2. Calculate x ^ n using x ^ n-1
+        int xpn = xpn1 * x;
+
+        // 3. Calculate x ^ n
+        return xpn;
+    }
+
+}
