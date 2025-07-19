d59ede2bc20e00eaad3aafbaef899aa63f83de02
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-07-19 22:37:57 +0530

    updated

---
+++ src/DSA_Level_1/E_Recursion/B_Array_Recursion/Q4_First_Index.java
@@ -0,0 +1,29 @@
+package DSA_Level_1.E_Recursion.B_Array_Recursion;
+
+import java.util.*;
+
+public class Q4_First_Index {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++)
+            arr[i] = scn.nextInt();
+
+        int x = scn.nextInt();
+        int ans = firstIndex(arr, 0, x);
+        System.out.println(ans);
+    }
+
+    public static int firstIndex(int[] arr, int idx, int x){
+        if(idx == arr.length) return -1;
+        if(arr[idx] == x) return idx;
+
+        int val = firstIndex(arr, idx + 1, x);
+        return val;
+
+    }
+
+}
