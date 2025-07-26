cd6443e98f4583c0b199c0fee5f69a362ce93822
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-07-26 10:50:08 +0530

    updated

--- src/DSA_Level_1/E_Recursion/D_Path_Problems/Q3_Print_Stair_Paths_2.java
+++ src/DSA_Level_1/E_Recursion/D_Path_Problems/Q3_Print_Stair_Paths_2.java
@@ -6,49 +6,31 @@ public class Q3_Print_Stair_Paths_2 {
 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-
-        ArrayList<String> res = getStairPaths(n);
-        System.out.println(res);
+        int input = scn.nextInt();
+        String output = "";
+        printStairPaths(input, output);
     }
 
-    public static ArrayList<String> getStairPaths(int n) {
-
-        if(n == 0)
+    public static void printStairPaths(int input,
+                                       String output) {
+        if(input == 0)
         {
-            // Positive Base Case : 1 Path Possible
-            ArrayList<String> paths = new ArrayList<>();
-            paths.add("");
-            return paths;
+            // Positive Base Case
+            System.out.println(output);
+            return;
         }
 
-        ArrayList<String> res = new ArrayList<>();
+        // Har kadam soch samajh kar rakha hai, khai me kabhi nahi girenge
+        // no need of negative base case, because edges leading to them are removed
 
-        // Only Edges leading to success are taken into account
-        // hence no need to handle failure (negative base case).
+        if(input - 1 >= 0)
+            printStairPaths(input - 1, output + 1);
 
-        if(n - 1 >= 0)
-        {
-            ArrayList<String> futurePaths1 = getStairPaths(n - 1);
-            for(String s1: futurePaths1)
-                res.add(1 + s1);
-        }
+        if(input - 2 >= 0)
+            printStairPaths(input - 2, output + 2);
 
-        if(n - 2 >= 0)
-        {
-            ArrayList<String> futurePaths2 = getStairPaths(n - 2);
-            for(String s2: futurePaths2)
-                res.add(2 + s2);
-        }
-
-        if(n - 3 >= 0)
-        {
-            ArrayList<String> futurePaths3 = getStairPaths(n - 3);
-            for(String s3: futurePaths3)
-                res.add(3 + s3);
-        }
-
-        return res;
+        if(input - 3 >= 0)
+            printStairPaths(input - 3, output + 3);
     }
 
 }
