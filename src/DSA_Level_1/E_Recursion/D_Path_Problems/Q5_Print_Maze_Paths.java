c71325e22d22d9a36cbac65e5522b86d8555dbdc
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-07-27 11:13:30 +0530

    updated

---
+++ src/DSA_Level_1/E_Recursion/D_Path_Problems/Q5_Print_Maze_Paths.java
@@ -0,0 +1,27 @@
+package DSA_Level_1.E_Recursion.D_Path_Problems;
+
+import java.io.*;
+
+public class Q5_Print_Maze_Paths {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        printMazePaths(0, 0, n - 1, m - 1, "");
+    }
+
+    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+        if(sr > dr || sc > dc){
+            return;
+        }
+
+        if(sr == dr && sc == dc){
+            System.out.println(psf);
+            return;
+        }
+
+        printMazePaths(sr, sc + 1, dr, dc, psf + "h");
+        printMazePaths(sr + 1, sc, dr, dc, psf + "v");
+    }
+}
