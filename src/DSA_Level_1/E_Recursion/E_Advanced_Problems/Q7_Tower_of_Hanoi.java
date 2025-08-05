0e4ed68568329fb2c504a3f5a083b39f9d40a44b
HariomSinghalPuri <www.singhalpuri143@gmail.com>
2025-08-05 08:09:22 +0530

    Tower of Hanoi Problem Solved

---
+++ src/DSA_Level_1/E_Recursion/E_Advanced_Problems/Q7_Tower_of_Hanoi.java
@@ -0,0 +1,31 @@
+package DSA_Level_1.E_Recursion.E_Advanced_Problems;
+
+import java.util.*;
+
+public class Q7_Tower_of_Hanoi {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int t1 = scn.nextInt();
+        int t2 = scn.nextInt();
+        int t3 = scn.nextInt();
+
+        toh(n, t1, t2, t3);
+    }
+
+    public static void toh(int n, int t1, int t2, int t3){
+        if(n == 0) return;
+
+        // 1. Faith n-1 disks from t1 to t3 using t2
+        toh(n-1, t1, t3, t2);
+
+        // 2. Move nth disk from t1 to t2
+        System.out.println(n + "[" + t1 + " -> " + t2 + "]");
+
+        // 3. Faith: n-1 disks from t3 to t2 using t1
+        toh(n-1, t3, t2, t1);
+    }
+
+}
